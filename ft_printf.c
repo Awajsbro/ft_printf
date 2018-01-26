2f5d24b470ce7e0dd943e34b016a1eedf65096fb
Arthur WAJSBROT <awajsbro@e1r11p10.42.fr>
2018-01-26 19:14:05 +0100

    maj 26/01/2018

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,61 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 17:55:44 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/01/13 18:25:55 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/01/26 19:13:19 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *s, ...)
+static char	*ft_wip(char *s, int *i, char *str, va_list va)
 {
+	*i = *i + 1;
+	if (s[*i] == 'c')
+		return (ft_memset(ft_strnew(1), va_arg(va, int), 1));
+	else if (s[*i] == 'i' || s[*i] == 'd')
+		return (ft_joinfree(ft_strndup(s, *i - 1), ft_itoa(va_arg(va, int))));
+	return ("");
+}
 
+static char	*pars(s, *i, str, va)
+{
+	if (s[i] == 0)
+		return (ft_strdup(s));
+	*i = *i + 1;
+	if (s[*i] == '%')
+		return (ft_memset(ft_strnew(1), '%', 1));
+
+}
+
+int			ft_printf(char constq *s, ...)
+{
+	va_list	va;
+	int		i;
+	char	*str;
+
+	va_start(va, s);
+	i = 0;
+	str = ft_strnew(0);
+	while (s[i] != 0)
+	{
+		while (!(s[i] == '%' && s[i + 1] != '%') && s[i] != 0)
+			i++;
+		str = ft_joinfree(str, ft_pars(s, &i, str, va));
+		s = s + i;
+		i = 0;
+	}
+	va_end(va);
+	ft_putstr(str);
+	len = len >= 0 ? ft_strlen(str) : -1;
+	return (len);
+}
+
+int			main(void)
+{
+	int		r;
+
+	r = ft_printf("%d\n", 2147483648);
+	printf("%d%c", 3, 'a');
+//	ft_putnbr(r);
+	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,26 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 17:56:46 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/01/13 18:25:14 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/01/26 13:23:47 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "include/libft.h"
+# include <stdarg.h>
+# include "libft/libft.h"
 
-int	ft_printf(const char *s, ...);
+typedef struct	s_arg
+{
+	coucou	truc;
+	char		flg;
+	int			wth;
+	int			acc;
+	char		*lgh;
+	void		*val;
+}				t_arg;
+
+int	ft_printf(char *s, ...);
 
 #endif
