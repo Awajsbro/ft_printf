19fddb5e61cdba1e54a8aa93c67dabfddb19924b
Arthur WAJSBROT <awajsbro@e1r11p2.42.fr>
2018-01-27 19:49:31 +0100

    27/01/2018

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,36 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 17:55:44 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/01/26 19:13:19 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/01/27 19:43:46 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static char	*ft_wip(char *s, int *i, char *str, va_list va)
+static char	*ft_wip(char const *s, int *i, char *str, va_list va)
 {
 	*i = *i + 1;
 	if (s[*i] == 'c')
-		return (ft_memset(ft_strnew(1), va_arg(va, int), 1));
+		return (ft_memset(ft_strnew(1), (va_arg(va, int) % 256), 1));
 	else if (s[*i] == 'i' || s[*i] == 'd')
 		return (ft_joinfree(ft_strndup(s, *i - 1), ft_itoa(va_arg(va, int))));
-	return ("");
+	return (ft_memalloc(0));
 }
 
-static char	*pars(s, *i, str, va)
+static char	*ft_pars(char const *s,int *i, char *str, va_list va)
 {
-	if (s[i] == 0)
+	if (s[*i] == 0)
 		return (ft_strdup(s));
-	*i = *i + 1;
-	if (s[*i] == '%')
-		return (ft_memset(ft_strnew(1), '%', 1));
-
+	return (ft_wip(s, i , str, va));
 }
-
-int			ft_printf(char constq *s, ...)
+//
+//static char	*ft_color(char const *s, int *i, char *str)
+//{
+//	static char gras;
+//	static char
+//}
+//
+int			ft_printf(char const *s, ...)
 {
 	va_list	va;
 	int		i;
@@ -43,24 +46,29 @@ int			ft_printf(char constq *s, ...)
 	str = ft_strnew(0);
 	while (s[i] != 0)
 	{
-		while (!(s[i] == '%' && s[i + 1] != '%') && s[i] != 0)
+		while (!(s[i] == '%' && s[i + 1] != '%') && s[i] != 0 && s[i] != '{')
 			i++;
-		str = ft_joinfree(str, ft_pars(s, &i, str, va));
-		s = s + i;
+		if (s[i] == '{')
+//			str = ft_color(s, &i, str);
+;
+		else
+			str = ft_joinfree(str, ft_pars(s, &i, str, va));
+		if (i == -42)
+			break;
+		s = s[i] != 0 ? s + i + 1 : s + i;
 		i = 0;
 	}
 	va_end(va);
 	ft_putstr(str);
-	len = len >= 0 ? ft_strlen(str) : -1;
-	return (len);
+	return (i == -42 ? ft_strlen(str) : -1);
 }
 
 int			main(void)
 {
 	int		r;
 
-	r = ft_printf("%d\n", 2147483648);
-	printf("%d%c", 3, 'a');
+	r = ft_printf("123||%d||abc -%c-\n", 2147483647, 310);
+	printf("%c\n", 310);
 //	ft_putnbr(r);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 17:56:46 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/01/26 13:23:47 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/01/27 19:49:06 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 
 typedef struct	s_arg
 {
-	coucou	truc;
 	char		flg;
 	int			wth;
 	int			acc;
@@ -26,6 +25,35 @@ typedef struct	s_arg
 	void		*val;
 }				t_arg;
 
-int	ft_printf(char *s, ...);
+int	ft_printf(char const *s, ...);
+
+/*
+** {\gras}		 =	\033[01m
+** {\soul}		 =	\033[04m
+** {\clig}		 =	\033[05m
+** {\noir}		 =	\033[30m
+** {\rouge}		 =	\033[31m
+** {\vert}		 =	\033[32m
+** {\jaune}		 =	\033[33m
+** {\bleu}		 =	\033[34m
+** {\rose}		 =	\033[35m
+** {\vert}		 =	\033[36m
+** {\vert}		 =	\033[37m
+** {\fond_blanc} =	\033[07m
+** {\fond_noir}	 =	\033[40m
+** {\fond_rouge} =	\033[41m
+** {\fond_vert}	 =	\033[42m
+** {\fond_jaune} =	\033[43m
+** {\fond_bleu}	 =	\033[44m
+** {\fond_rose}	 =	\033[45m
+** {\fond_cyan}	 =	\033[46m
+** {\fond_gris}	 =	\033[47m
+** {\true_reset} =	\033[00m
+** {\fond_reset}
+** {\colo_reset}
+** {\gras_reset}
+** {\soul_reset}
+** {\clig_reset}
+*/
 
 #endif
