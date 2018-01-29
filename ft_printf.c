c797f68088bce80921e7878e80760863d5c899a2
Arthur WAJSBROT <awajsbro@e1r12p18.42.fr>
2018-01-29 19:11:24 +0100

    maj du 29/01/2018

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,13 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 17:55:44 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/01/27 19:43:46 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/01/29 19:08:37 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+/*
 static char	*ft_wip(char const *s, int *i, char *str, va_list va)
 {
 	*i = *i + 1;
@@ -61,6 +62,97 @@ int			ft_printf(char const *s, ...)
 	va_end(va);
 	ft_putstr(str);
 	return (i == -42 ? ft_strlen(str) : -1);
+}
+*/
+static size_t	ft_color(char const *s, int *i, short color)
+{
+	if (ft_strnequ(s, "{\rouge}", 7) == 1)
+	{
+		write(1, "\033[31m", 5);
+		color = ((color / 10) * 10) + 1);
+	}
+	else if (ft_strnequ(s, "{\noir}", 6) == 1)
+	{
+		write(1, "\033[30m", 5);
+		color = ((color / 10) * 10) + 0);
+	}
+	else if (ft_strnequ(s, "{\vert}", 6) == 1)
+	{
+		write(1, "\033[32m", 5);
+		color = ((color / 10) * 10) + 2);
+	}
+	else if (ft_strnequ(s, "{\bleu}", 6) == 1)
+	{
+		write(1, "\033[34m", 5);
+		color = ((color / 10) * 10) + 4);
+	}
+	else if (ft_strnequ(s, "{\rose}", 6) == 1)
+	{
+		write(1, "\033[35m", 5);
+		color = ((color / 10) * 10) + 5);
+	}
+	else
+		return (ft_more_color(s, i));
+	*i = *i + 6;
+	return (7);
+}
+
+static size_t	ft_pars_color(char const *s, int *i)
+{
+	static	char	color = 99;
+	static	char	state = 0;
+
+	if (ft_strnequ(s[*i], "{\reset_", 7) == 1)
+	{
+		*i = *i + 7;
+		return (ft_reset_color(s, i, color, state));
+	}
+	else if (ft_strnequ(s[*i], "{\state_", 8) == 1)
+	{
+		*i = *i + 8;
+		return (ft_change_state(s, i, state));
+	}
+	else if (ft_strnequ(s[*i], "{\fond_", 6) == 1)
+	{
+		*i = *i + 6;
+		return (ft_change_back(s, i, color, state));
+	}
+	else
+		return (ft_change_color(s, i, color));
+}
+
+static size_t	ft_pars(char const *s, int *i, va_list va)
+{
+	*i = *i + 1;
+	if (s[*i] == '%')
+		return (0);
+	else if (s[*i] == '{')
+		return (ft_pars_color(s, i));
+	else
+		*i = -42;
+	return (0);
+}
+
+int		ft_printf(char const *s, ...)
+{
+	va_list		va;
+	int			i;
+	long long	len;
+
+	len = 0;
+	va_start(va, s);
+	while (s[0] != 0)
+	{
+		i = ft_strclen(s, '%');
+		len = len + ft_putstrn(s, i));
+		if (s[i] == '%')
+			len = len + ft_pars(s, &i, va);
+		if (i == -42);
+			break;
+		s = s + i;
+	}
+	va_end(va);
+	return (i == -42 ? -1 : len);
 }
 
 int			main(void)
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 17:56:46 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/01/27 19:49:06 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/01/29 19:08:39 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,17 @@ typedef struct	s_arg
 int	ft_printf(char const *s, ...);
 
 /*
-** {\gras}		 =	\033[01m
-** {\soul}		 =	\033[04m
-** {\clig}		 =	\033[05m
-** {\noir}		 =	\033[30m
+** {\state_gras} =	\033[01m
+** {\state_soul} =	\033[04m
+** {\state_clig} =	\033[05m
+** {\noir}		 =	\033[30m --
 ** {\rouge}		 =	\033[31m
-** {\vert}		 =	\033[32m
+** {\vert}		 =	\033[32m --
 ** {\jaune}		 =	\033[33m
-** {\bleu}		 =	\033[34m
-** {\rose}		 =	\033[35m
-** {\vert}		 =	\033[36m
-** {\vert}		 =	\033[37m
+** {\bleu}		 =	\033[34m --
+** {\rose}		 =	\033[35m --
+** {\cyan}		 =	\033[36m
+** {\gris}		 =	\033[37m
 ** {\fond_blanc} =	\033[07m
 ** {\fond_noir}	 =	\033[40m
 ** {\fond_rouge} =	\033[41m
@@ -48,12 +48,12 @@ int	ft_printf(char const *s, ...);
 ** {\fond_rose}	 =	\033[45m
 ** {\fond_cyan}	 =	\033[46m
 ** {\fond_gris}	 =	\033[47m
-** {\true_reset} =	\033[00m
-** {\fond_reset}
-** {\colo_reset}
-** {\gras_reset}
-** {\soul_reset}
-** {\clig_reset}
+** {\reset_true} =	\033[00m
+** {\reset_fond}
+** {\reset_colo}
+** {\reset_gras}
+** {\reset_soul}
+** {\reset_clig}
 */
 
 #endif
